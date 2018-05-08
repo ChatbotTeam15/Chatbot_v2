a8c9d1a42c5a494003a30e11a6ccc4085789a490
Shixiang Wang <ftg1161@autuni.ac.nz>
2018-05-08 19:12:35 +1200

    添加项目文件。

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=chatbotv2-Web-Deploy.pubxml -p:Password=iTz3gHqm0PXH6fxksa7AZsiovzTNJWk6jd56thetadRRjEwZrroZ8Tzt8Bwq
+
