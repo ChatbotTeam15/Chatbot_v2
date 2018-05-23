e779b36f06e1574b8d7b97755ba75017f70c6b35
Shixiang Wang <ftg1161@autuni.ac.nz>
2018-05-23 17:13:50 +1200

    add questions can be ask

--- Dialogs/BasicQnAMakerDialog.cs
+++ Dialogs/BasicQnAMakerDialog.cs
@@ -14,9 +14,16 @@ namespace Microsoft.Bot.Sample.QnABot
     {
         public async Task StartAsync(IDialogContext context)
         {
+            
             /* Wait until the first message is received from the conversation and call MessageReceviedAsync 
             *  to process that message. */
-            context.PostAsync("What would you like to know about the BCIS papers?");
+            await context.PostAsync("You can ask me following question:\nWhat are the pre-requisites and co- requisites for <a specific paper>\n"+
+            "If I take a < specific paper > what other papers should I take next for < the software  dev major in the BCIS >\n"+
+             "What would be a suggested set of papers for a < the software  dev  major in the BCIS >?\n" +
+             "Which papers are suitable for a < specific job > (like web developer, business analyst, software engineer, scrum master)\n" +
+            "If I have failed<specific paper> what papers can I still take ? (or how does this restrict what papers I can take)\n" +
+            "What semesters is < specific paper > offered in < specific year > ");
+            //context.Wait(this.MessageReceivedAsync);
             context.Wait(this.MessageReceivedAsync);
         }
 
